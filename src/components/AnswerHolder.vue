533e1c85835a6deaad1b9baa12cc06c634def846
LeppoXena <francesco.lepore@xenagames.com>
2023-04-03 23:02:12 +0200

    Selection animation on answer

--- src/components/AnswerHolder.vue
+++ src/components/AnswerHolder.vue
@@ -3,13 +3,13 @@
     <div class="container">      
       <div class="answer">
         <i class="label-left"></i>
-        <p class="label" @click="confirmAns(answers.a1_check)">
+        <p class="label" @click="confirmAns(answers.a1_check,1)" v-bind:class="selectedAns == 1 && checkColor === true ?'choice':''">
           {{"A: " + answers.a1 }}
         </p>
         <i class="label-right"></i>
         <i class="label-line"></i>
         <i class="label-left"></i>
-        <p class="label" @click="confirmAns(answers.a2_check)">
+        <p class="label" @click="confirmAns(answers.a2_check,2)" v-bind:class="selectedAns == 2 && checkColor === true ?'choice':''">
           {{"B: " + answers.a2 }}
         </p>
         <i class="label-right"></i>
@@ -18,13 +18,13 @@
     <div class="container">
       <div class="answer">
         <i class="label-left"></i>
-        <p class="label" @click="confirmAns(answers.a3_check)">
+        <p class="label" @click="confirmAns(answers.a3_check,3)" v-bind:class="selectedAns == 3 && checkColor === true ?'choice':''">
           {{"C: " + answers.a3 }}
         </p>
         <i class="label-right"></i>
         <i class="label-line"></i>
         <i class="label-left"></i>
-        <p class="label" @click="confirmAns(answers.a4_check)">
+        <p class="label" @click="confirmAns(answers.a4_check,4)" v-bind:class="selectedAns == 4 && checkColor === true ?'choice':''">
           {{"D: " +  answers.a4 }}
         </p>
         <i class="label-right"></i>
@@ -41,6 +41,8 @@ export default {
   name: 'AnswerHolder',
   data(){
     return{
+      checkColor : null,
+      selectedAns : null,
       score : 0,
       price : [
       "500 €",
@@ -58,7 +60,8 @@ export default {
       "150.000 €",
       "300.000 €",
       "1.000.000 €",
-      "HAI VINTO!"
+      "HAI VINTO 1 MILIONE DI EURO!",
+      "Che peccato, Hai perso!"
       ]
     }
   },
@@ -66,16 +69,33 @@ export default {
     answers: Object,
   },
   methods: {
-    confirmAns(answer) {
-      if (answer == true) {
-        console.log("risposta corretta " + answer);
-        this.$emit('correct-ans');
-        return this.score++
+    confirmAns(check,ansNum) {
+      this.selectedAns=ansNum;
+      this.checkColor=check;
+      console.log("risposta scelta " + this.selectedAns);
+      if (check == true) {
+        setTimeout(() => {
+          console.log("risposta corretta " + check);
+        }, 2000);
+        setTimeout(() => {
+          this.$emit('correct-ans');
+          this.selectedAns=null;
+          this.checkColor=null;
+
+          this.score++
+        }, 3000);
       }
       else {
-        console.log("risposta errate " + answer);
-        this.$emit('wrong-ans');
-        return this.score=0
+        setTimeout(() => {
+          console.log("risposta errata " + check);
+          this.score=16;
+        }, 2000);
+        setTimeout(() => {
+          this.$emit('wrong-ans');
+          this.selectedAns=null;
+          this.checkColor=null;
+          this.score=0
+        }, 3000);
       }
     }
   }
@@ -105,12 +125,16 @@ export default {
     justify-content: center;
   }
   .label{
+    position: relative;
+    cursor:pointer;
     text-align: center;
+    text-decoration: none;
     flex-grow: 1;
     min-width: 35%;
     display: inline-block;
     background-color: #6a00ff;
     padding: 8px;
+    transition-duration: 0.4s;
   }
   .label-left {
     display: inline-block;
@@ -131,6 +155,70 @@ export default {
     display: inline-block;
     width: 10%;
   }
+  .label.selected {
+  background-color: red;
+}
+
+.label:hover {
+  transition-duration: 0.1s;
+  background-color: rgb(140, 127, 255);
+  filter:brightness(0,6);
+}
+
+.label:after {
+  content: "";
+  display: block;
+  position: absolute;
+  border-radius: 4em;
+  left: 0;
+  top:0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: all 0.5s;
+  box-shadow: 0 0 10px 40px white;
+}
+
+.label:active:after {
+  box-shadow: 0 0 0 0 white;
+  position: absolute;
+  left: 0;
+  top:0;
+  opacity: 1;
+  transition: 0s;
+}
+
+.button:active {
+  top: 1px;
+}
+  .choice {
+  animation: clicked 3s;
+}
+@keyframes clicked {
+  0% {
+    background-color: #6a00ff;
+  }
+  66% {
+    background-color: #6a00ff;
+  }
+  75% {
+    background-color: green;
+  }
+  100% {
+    background-color: green;
+  }
+}
+// @keyframes clickedWrong {
+//   0% {
+//     background-color: #6a00ff;
+//   }
+//   50% {
+//     background-color: red;
+//   }
+//   100% {
+//     background-color: #6a00ff;
+//   }
+// }
 
 
 </style>
